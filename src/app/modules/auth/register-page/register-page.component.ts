d44637831ce06224c4714aa24539cd4a2b65e165
ashina <ashina@riota.in>
2024-08-12 22:37:51 +0530

    12-08

--- src/app/modules/auth/register-page/register-page.component.ts
+++ src/app/modules/auth/register-page/register-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,8 +10,35 @@ import { Router } from '@angular/router';
 export class RegisterPageComponent {
   constructor(private formBuilder: FormBuilder, private router: Router) {}
 
-  profileForm = this.formBuilder.group({
-    username: ['', Validators.required, Validators.email],
-    password: ['', Validators.required],
-  });
+  profileForm = this.formBuilder.group(
+    {
+      name: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      mobileNO: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    },
+    {
+      validator: this.ConfirmPasswordValidator('password', 'confirmPassword'),
+    }
+  );
+  isActive = true;
+
+  ConfirmPasswordValidator(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      let control = formGroup.controls[controlName];
+      let matchingControl = formGroup.controls[matchingControlName];
+      if (
+        matchingControl.errors &&
+        !matchingControl.errors['confirmPasswordValidator']
+      ) {
+        return;
+      }
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ confirmPasswordValidator: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
 }
