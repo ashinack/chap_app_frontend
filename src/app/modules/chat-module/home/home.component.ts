ac8a14d441d9b4fff40cfaf01e63f7868a20839f
ashina <ashina@riota.in>
2024-08-20 22:30:08 +0530

    menu structure

--- src/app/modules/chat-module/home/home.component.ts
+++ src/app/modules/chat-module/home/home.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  menuList: any[] = [
+    { name: 'Dashboard', icon: 'dashboard' },
+    { name: 'Chat', icon: 'forum' },
+    { name: 'Users', icon: 'account_circle' },
+    { name: 'Calender', icon: 'calendar_month' },
+    { name: 'Friend', icon: 'diversity_3' },
+  ];
 }
