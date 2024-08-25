c5a1fddc7f70e844ef3b956b305081ae1c1d05e2
ashina <ashina@riota.in>
2024-08-25 22:10:35 +0530

    chat-page ui

--- src/app/modules/chat-module/chat-page/chat-page.component.ts
+++ src/app/modules/chat-module/chat-page/chat-page.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-chat-page',
   templateUrl: './chat-page.component.html',
-  styleUrl: './chat-page.component.scss'
+  styleUrl: './chat-page.component.scss',
 })
 export class ChatPageComponent {
-
+  chatList: any[] = [1, 2, 3, 4, 5];
 }
